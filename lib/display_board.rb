8f4207bc29879aaebd42ee702a9e1ec48ecc6394
Matias Damonte <mdamonte88@gmail.com>
2019-12-30 03:17:21 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,10 +1,15 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
 def display_board
+  puts "Welcome to Tic Tac Toe"
   for i in 1..3 do
     if i > 1
       puts "-----------"
     end
-    puts "   |   |   "
+    if i * 3 > gets.chomp 
+
+    else
+      puts "   |   |   "
+    end
   end
 end
 
